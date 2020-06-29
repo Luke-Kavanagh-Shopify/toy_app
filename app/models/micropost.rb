3dfeec5683c239bf4389a1438307a3b6755be91f
Ubuntu <ubuntu@ip-172-31-10-10.ap-southeast-2.compute.internal>
2020-06-29 05:06:23 +0000

    added validation to user name and email props

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,5 @@
 class Micropost < ApplicationRecord
-end
+    belongs_to :user
+    validates :content, length: { maximum: 140 }
+    validates :name, presence: true    
+    validates :email , presence: true    
